dbc4975da0b4a030c42d831e5eefe1d3c6483afc
zarutuzuchave <rhbatwon@gmail.com>
2026-05-21 07:15:19 -0300

    ex1

---
+++ lista 1/ex1.py
@@ -0,0 +1,20 @@
+# Explique o que são tipos de dados. Apresente pelo menos três exemplos de como
+# são utilizados.
+
+# Tipos de dados são a natureza que um dado ele será armazenado em uma variavel e quais operções podem ser feitas com ele.
+# O python ele é uma linguagem com linguagem dinamica, ou seja, Você não precisa declarar o tipo da variável (como int ou str) ao criá-la.
+# O interpretador reconhece o tipo automaticamente em tempo de execução e o tipo pode mudar se você atribuir um novo valor.
+# Ex: Int
+variavel = int(input("digite um valor inteiro: "))
+#Isso define que somente valores do tipo INTEIRO (INT) possam ser inseridos na variavel, se for digitado um número com virgula ou uma string ele mostrará erro.
+# Ex: Float
+variavel2 = float(input("digite um valor com ponto: "))
+# Aqui somente valores com virgula (no caso da computação ponto) sejam inseridos.
+# Ex: Str
+variavel3 = str(input("digite um nome: "))
+#A string (str) é o tipo usado para textos, somente letras , palavras ou frases seram aceitas.
+
+# Os tipos INT e FLOAT são usados para números, no caso do INT somente numeros inteiros sem nenhuma casa decimal, já no FLOAT somente numeros com casas decimais ou seja virgula(ponto).
+# Já a STRING é um tipo para caracteres do tipo texto, se for digitado um número ela apresentará um erro.
+# Esses tipos de dados são os mais comuns na programação e os mais básicos, existem outros como boolean, list, dict e outros que tambem são usados na programação.
+  
