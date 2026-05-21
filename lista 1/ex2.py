85f4b848b9896a2d866fe86e903e06c6dd179670
zarutuzuchave <rhbatwon@gmail.com>
2026-05-21 07:37:42 -0300

    fim do ex 2 e 3

---
+++ lista 1/ex2.py
@@ -0,0 +1,14 @@
+# Explique o funcionamento dos comandos print e input. Dê ao menos três
+# exemplos de cada
+
+# Print: o comando print é muito usado na programação porque ele mostra qualquer coisa no terminal, ele imprime (print em ingles) o que for pedido dentro dele.
+# Exemplo 1:
+print("Olá mundo")
+# Aqui nos pedimos para que seja impresso a frase "Olá mundo", o print irá converter automaticamente para str o que foi digitado dentro se estiver com "" do contrario ele irá dar procurar uma variavel com o mesmo nome para executar.
+# Exemplo 2:
+variavel = "Sou uma variavel @_@"
+print(variavel)
+# Nesse exemplo o print irá mostrar no terminal o conteudo que foi armazenado na variavel.
+# Exemplo 3:
+print(2+1)
+# Também é possivel executar contas simples dentro do print ele as realiza de forma rápida. Se você não usar "", o computador fará a conta matemática primeiro e exibirá apenas o resultado.
---
+++ lista 1/ex3.py
@@ -0,0 +1,19 @@
+a = (4/2) + (2/4)
+aa = 4/2 + 2/4
+b = (4+2) * 2 - 4
+bb = 4+2 *  2 - 4
+
+print(a)
+print(aa)
+print(b)
+print(bb)
+# No par a e aa os resultados são iguais.
+# No Python, a divisão (/) tem precedência sobre a adição (+).No primeiro caso, os parênteses apenas reforçam o que o Python já faria naturalmente.
+# No segundo caso, o Python resolve primeiro as divisões (4/2 e 2/4) e só depois soma os resultados.
+# No par b e bb os resultados são diferentes.
+# No Python, a presença de parênteses altera a ordem de precedência das operações aritméticas.
+# Na  instrução b os parentes tem maior prioridade de calculo
+# No par bb a linguagem segue a regra padrão de precedência de operadores do Python:
+# Multiplicação (maior prioridade que soma/subtração):
+# Soma (da esquerda para a direita): 
+# Subtração: 
