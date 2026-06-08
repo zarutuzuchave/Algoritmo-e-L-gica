3e959ef462cfa7958bbc3c6c5c4b33c5d786a407
Filipe.Silva <126197433+zarutuzuchave@users.noreply.github.com>
2026-06-08 11:14:31 -0300

    Update lista4_ex5.py

--- lista 4/lista4_ex5.py
+++ lista 4/lista4_ex5.py
@@ -2,14 +2,18 @@
 # quando ele é igual a soma dos seus divisores excetuando ele próprio. (Ex: 6 é perfeito,6 = 1 + 2 + 3, que são seus divisores).
 # A função deve retornar o valor inteiro 1 para verdadeiro e 0 caso contrário.
 
+def numero_perfeito(n):
+    soma = 0
+
+    for i in range(1, n):
+        if n % i == 0:
+            soma += i
+
+    if soma == n:
+        print("Número perfeito")
+    else:
+        print("Não é um número perfeito")
+
+
 n = int(input("Digite um número: "))
-soma = 0
-
-for i in range(1, n):
-    if n % i == 0:
-        soma += i
-
-if soma == n:
-    print("Número perfeito")
-else:
-    print("Não é um número perfeito")
+numero_perfeito(n)
