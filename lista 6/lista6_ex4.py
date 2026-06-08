932b30a7063d1a359733a1f0fc53c4773c9731e4
Filipe.Silva <126197433+zarutuzuchave@users.noreply.github.com>
2026-06-08 11:51:22 -0300

    Update and rename ex4.py to lista6_ex4.py

--- lista 6/lista6_ex4.py
+++ lista 6/lista6_ex4.py
@@ -2,13 +2,12 @@
 # utilizando um laço de repetição, receba os valores de entrada e, utilizando outro laço
 # de repetição, verifique qual o maior valor da lista e apresente esse valor.
 lista = []
+
 for i in range(8):
-    entrada = int(input("digite os valores de entrada: "))
+    entrada = int(input("Digite os valores de entrada: "))
     lista.append(entrada)
 maior = lista[0]
-for i in range(8):
-    if lista[i]> maior:
-        maior = lista
-    print(maior)
-
-
+for numero in lista:
+    if numero > maior:
+        maior = numero
+print(maior)
