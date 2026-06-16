3a1bf453f0adfe08316947e3f7f4e185e13060ec
zarutuzuchave <rhbatwon@gmail.com>
2026-06-16 16:38:41 -0300

    alteraçao

--- lista 6/lista6_ex5.py
+++ lista 6/lista6_ex5.py
@@ -2,7 +2,6 @@
 #   utilizando um laço de repetição,e utilizando outro laço, inicialize os valores de ambas
 #   as listas, some os valores posição por posição e guarde o novo valor na lista A.
 
-<<<<<<< HEAD
 lista_a = []
 lista_b = []
 
@@ -16,14 +15,3 @@ for i in range(6):
     
 
 print(lista_a)
-=======
-a = []
-b = []
-
-for i in range(6):
-    a.append(float(input("Digite um valor para A: ")))
-for i in range(6):
-    b.append(float(input("Digite um valor para B: ")))
-for i in range(6):
-    a[i] += b[i]
->>>>>>> e0e1d35152e71028541d9aa62b6ee4de73d96190
